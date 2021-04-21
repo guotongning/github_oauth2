74168b12a28693124b991706fbf0b537b511a19b
guotongning <guotongning@58.com>
2021-04-21 11:11:30 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Github OAuth2 Login Application
+
+> 我的oauth2登陆服务
+> github登陆第三方服务
+
---
+++ src/main/java/com/ning/geekbang/github_oauth2/GithubOauth2Application.java
@@ -0,0 +1,13 @@
+package com.ning.geekbang.github_oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubOauth2Application.class, args);
+    }
+
+}
